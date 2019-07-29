34d1695e2dd8c32a9cc36b3dda88b4bcca90973e
Liam Aharon <liam.aharon@hotmail.com>
2019-07-29 13:33:20 +1000

    Add assist example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
+import Web3 from 'web3';
+import assist from 'bnc-assist';
+import { address, abi } from './contract-details';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    const web3 = new Web3(window.ethereum);
+
+    const bncAssistConfig = {
+      dappId: '15273e72873dba2cd065',
+      networkId: 4,
+      web3
+    };
+
+    const assistInstance = assist.init(bncAssistConfig);
+
+    await assistInstance.onboard();
+    const contract = new web3.eth.Contract(abi, address);
+
+    const decorated = assistInstance.Contract(contract);
+
+    this.state = { decorated, web3 };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button
+            onClick={() =>
+              this.state.decorated.methods
+                .highFive('0x6921144ad2a2923ae11c5652feb3b0d9053d9152')
+                .send({ from: window.ethereum.selectedAddress })
+            }
+          >
+            High five
+          </button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/contract-details.js
@@ -0,0 +1,77 @@
+export const address = '0x9e7ae8c4d07670df36fdb88c1b3ae06dc4625fa0';
+
+export const abi = [
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'address' }],
+    name: 'votes',
+    outputs: [{ name: '', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: false, name: '_sender', type: 'address' },
+      { indexed: false, name: '_recipient', type: 'address' }
+    ],
+    name: 'HighFive',
+    type: 'event'
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: false, name: '_voter', type: 'address' },
+      { indexed: false, name: '_recipient', type: 'address' },
+      { indexed: false, name: '_voteTotal', type: 'uint256' }
+    ],
+    name: 'VoteUp',
+    type: 'event'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'failHard',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'failOOG',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'failHalf',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [{ name: '_recipient', type: 'address' }],
+    name: 'highFive',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  },
+  {
+    constant: false,
+    inputs: [{ name: '_recipient', type: 'address' }],
+    name: 'voteUp',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function'
+  }
+];
